1dad06298e12c4be809992ed4f13756d4acdd1b2
mack <1103931738@qq.com>
2025-10-29 17:35:22 +0800

    提交

--- src/main/java/java/service/RedundantDataService.java
+++
@@ -1,9 +0,0 @@
-package java.service;
-
-import java.dto.RedundantDataRequest;
-import java.entity.RedundantDataRecord;
-
-public interface RedundantDataService {
-
-    RedundantDataRecord exportRedundantData(RedundantDataRequest request);
-}
